7c0134da23b29463233bd45bf22058172433fd54
Rafael Martins <rafaeldcmartins@gmail.com>
2020-04-01 21:23:48 -0300

    Added ideas.html, updated files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 function onOff() {
    document.querySelector('#modal')
    .classList.toggle('hide')
+
+   document.querySelector('body')
+   .classList.toggle('hideScroll')
+
+   document.querySelector('#modal')
+   .classList.toggle('addScroll')
 }
